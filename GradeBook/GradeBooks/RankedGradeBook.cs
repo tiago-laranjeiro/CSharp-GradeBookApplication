b005e1e6b3a327c31c8f80fd51740b59c3f58488
tiago-laranjeiro <tiago.laranjeiro@avanade.com>
2019-11-08 13:45:04 +0100

    Second part

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,39 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[threshold-1])
+            {
+                return 'A';
+            }
+            else if (averageGrade >= grades[threshold*2-1])
+            {
+                return 'B';
+            }
+            else if (averageGrade >= grades[threshold * 2 - 1])
+            {
+                return 'C';
+            }
+            else if (averageGrade >= grades[threshold * 3 - 1])
+            {
+                return 'D';
+            }
+            else if (averageGrade >= grades[threshold * 4 - 1])
+            {
+                return 'E';
+            }
+            return 'F';
         }
     }
 }
