20e3a3ae7f20bff60b0bbfb65baf2ada93c555c6
tiago-laranjeiro <tiago.laranjeiro@avanade.com>
2019-11-08 13:21:11 +0100

    First part

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            this.Type = GradeBookType.Standard;
+        }
+
+        public override void CalculateStatistics()
+        {
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            base.CalculateStudentStatistics(name);
+        }
+
+        public override double GetGPA(char letterGrade, StudentType studentType)
+        {
+            return base.GetGPA(letterGrade, studentType);
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            return base.GetLetterGrade(averageGrade);
+        }
+    }
+}
