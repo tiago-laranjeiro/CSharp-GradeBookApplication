b0c12fff9e946156385c00c40f7f1f7f79bf221f
tiago-laranjeiro <tiago.laranjeiro@avanade.com>
2019-11-08 13:23:24 +0100

    First part - second try

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -5,7 +5,7 @@ using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    class StandardGradeBook : BaseGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
         public StandardGradeBook(string name) : base(name)
         {
